fbef75f03835169d81f5bcc9d460a1b0d4d6e3e4
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-11-26 13:02:26 +0900

    update train.py

--- train.py
+++ train.py
@@ -1,29 +1,127 @@
-import os
-from detectron2 import model_zoo
-from detectron2.engine import DefaultTrainer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.data.datasets import load_voc_instances
+from mxnet import autograd, gluon, image, init, np, npx
+from mxnet.gluon import utils, nn, Parameter
 
-# データの登録
-DatasetCatalog.register('my_dataset', lambda: load_voc_instances('train_data', 'train', ['target', 'green']))
-MetadataCatalog.get('my_dataset').thing_classes = ['target', 'green']
 
-# モデルの取得
-model = 'COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml'
-cfg = model_zoo.get_config(model, trained=True)
+# Reading the Content and Style Images
+content_img_fname = utils.download('https://github.com/d2l-ai/d2l-en/raw/master/img/rainier.jpg')
+content_img = image.imread(content_img_fname)
 
-# 訓練データをセット
-cfg.DATASETS.TRAIN = ('my_dataset',)
-# テストデータをセット（今回はセットしない）
-cfg.DATASETS.TEST = () 
+style_img_fname = utils.download('https://github.com/d2l-ai/d2l-en/raw/master/img/autumn-oak.jpg')
+style_img = image.imread(style_img_fname)
 
-cfg.DATALOADER.NUM_WORKERS = 4                  # default:4
-cfg.SOLVER.IMS_PER_BATCH = 2                    # default:16
-cfg.SOLVER.BASE_LR = 0.00025                    # dafault:0.02
-cfg.SOLVER.MAX_ITER = 1000                      # default:270000
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32   # dafault:512 
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2             # dafault:80
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg)
-trainer.train()
+# Preprocessing and Postprocessing
+rgb_mean = np.array([0.485, 0.456, 0.406])
+rgb_std = np.array([0.229, 0.224, 0.225])
+
+def preprocess(img, image_shape):
+    img = image.imresize(img, *image_shape)
+    img = (img.astype('float32') / 255 - rgb_mean) / rgb_std
+    return np.expand_dims(img.transpose(2, 0, 1), axis=0)
+
+def postprocess(img):
+    img = img[0].as_in_ctx(rgb_std.ctx)
+    return (img.transpose(1, 2, 0) * rgb_std + rgb_mean).clip(0, 1)
+
+
+# Extracting Features
+pretrained_net = gluon.model_zoo.vision.vgg19(pretrained=True)
+style_layers, content_layers = [0, 5, 10, 19, 28], [25]
+
+net = nn.Sequential()
+for i in range(max(content_layers + style_layers) + 1):
+    net.add(pretrained_net.features[i])
+
+def extract_features(X, content_layers, style_layers):
+    contents = []
+    styles = []
+    for i in range(len(net)):
+        X = net[i](X)
+        if i in style_layers:
+            styles.append(X)
+        if i in content_layers:
+            contents.append(X)
+    return contents, styles
+
+def get_contents(image_shape, device):
+    content_X = preprocess(content_img, image_shape).copyto(device)
+    contents_Y, _ = extract_features(content_X, content_layers, style_layers)
+    return content_X, contents_Y
+
+def get_styles(image_shape, device):
+    style_X = preprocess(style_img, image_shape).copyto(device)
+    _, styles_Y = extract_features(style_X, content_layers, style_layers)
+    return style_X, styles_Y
+
+
+# Defining the Loss Function
+def content_loss(Y_hat, Y):
+    return np.square(Y_hat - Y).mean()
+def gram(X):
+    num_channels, n = X.shape[1], X.size // X.shape[1]
+    X = X.reshape((num_channels, n))
+    return np.dot(X, X.T) / (num_channels * n)
+
+def style_loss(Y_hat, gram_Y):
+    return np.square(gram(Y_hat) - gram_Y).mean()
+
+def tv_loss(Y_hat):
+    return 0.5 * (np.abs(Y_hat[:, :, 1:, :] - Y_hat[:, :, :-1, :]).mean() +
+                  np.abs(Y_hat[:, :, :, 1:] - Y_hat[:, :, :, :-1]).mean())
+
+content_weight, style_weight, tv_weight = 1, 1e3, 10
+
+def compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram):
+    # Calculate the content, style, and total variance losses respectively
+    contents_l = [
+        content_loss(Y_hat, Y) * content_weight
+        for Y_hat, Y in zip(contents_Y_hat, contents_Y)]
+    styles_l = [
+        style_loss(Y_hat, Y) * style_weight
+        for Y_hat, Y in zip(styles_Y_hat, styles_Y_gram)]
+    tv_l = tv_loss(X) * tv_weight
+    # Add up all the losses
+    l = sum(10 * styles_l + contents_l + [tv_l])
+    return contents_l, styles_l, tv_l, l
+
+
+# Initializing the Synthesized Image
+class SynthesizedImage(nn.Block):
+    def __init__(self, img_shape, **kwargs):
+        super(SynthesizedImage, self).__init__(**kwargs)
+        self.weight = Parameter('weight', shape=img_shape)
+
+    def forward(self):
+        return self.weight.data()
+
+def get_inits(X, device, lr, styles_Y):
+    gen_img = SynthesizedImage(X.shape)
+    gen_img.initialize(init.Constant(X), device=device, force_reinit=True)
+    trainer = gluon.Trainer(gen_img.collect_params(), 'adam',
+                            {'learning_rate': lr})
+    styles_Y_gram = [gram(Y) for Y in styles_Y]
+    return gen_img(), styles_Y_gram, trainer
+
+
+#Training
+def train(X, contents_Y, styles_Y, device, lr, num_epochs, lr_decay_epoch):
+    X, styles_Y_gram, trainer = get_inits(X, device, lr, styles_Y)
+    for epoch in range(num_epochs):
+        with autograd.record():
+            contents_Y_hat, styles_Y_hat = extract_features(
+                X, content_layers, style_layers)
+            contents_l, styles_l, tv_l, l = compute_loss(
+                X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram)
+        l.backward()
+        trainer.step(1)
+        if (epoch + 1) % lr_decay_epoch == 0:
+            trainer.set_learning_rate(trainer.learning_rate * 0.8)
+    return X
+
+
+device = npx.gpu() if npx.num_gpus() > 0 else npx.cpu()
+image_shape = (450, 300)
+net.reset_device(device)
+content_X, contents_Y = get_contents(image_shape, device)
+_, styles_Y = get_styles(image_shape, device)
+output = train(content_X, contents_Y, styles_Y, device, 0.9, 500, 50)

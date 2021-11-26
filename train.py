d9561a79032e38490ad4986179a1e0918174f477
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-11-26 21:23:30 +0900

    update train.py

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from mxnet import autograd, gluon, image, init, np, npx
 from mxnet.gluon import utils, nn, Parameter
+from PIL import Image
 
 
 # Reading the Content and Style Images
@@ -19,10 +20,11 @@ def preprocess(img, image_shape):
     img = (img.astype('float32') / 255 - rgb_mean) / rgb_std
     return np.expand_dims(img.transpose(2, 0, 1), axis=0)
 
-def postprocess(img):
-    img = img[0].to_device(rgb_std.device)
-    return (img.transpose(1, 2, 0) * rgb_std + rgb_mean).clip(0, 1)
-
+def array_to_pil(img):
+    img = img.to_device(npx.cpu())
+    img = np.transpose(img[0], (1, 2, 0))
+    img_array =  ((img * rgb_std + rgb_mean) * 255).asnumpy().clip(0, 255).astype('uint8')
+    return Image.fromarray(img_array)
 
 # Extracting Features
 pretrained_net = gluon.model_zoo.vision.vgg19(pretrained=True)
@@ -44,12 +46,12 @@ def extract_features(X, content_layers, style_layers):
     return contents, styles
 
 def get_contents(image_shape, device):
-    content_X = preprocess(content_img, image_shape).copyto(device)
+    content_X = preprocess(content_img, image_shape).to_device(device)
     contents_Y, _ = extract_features(content_X, content_layers, style_layers)
     return content_X, contents_Y
 
 def get_styles(image_shape, device):
-    style_X = preprocess(style_img, image_shape).copyto(device)
+    style_X = preprocess(style_img, image_shape).to_device(device)
     _, styles_Y = extract_features(style_X, content_layers, style_layers)
     return style_X, styles_Y
 
@@ -57,6 +59,7 @@ def get_styles(image_shape, device):
 # Defining the Loss Function
 def content_loss(Y_hat, Y):
     return np.square(Y_hat - Y).mean()
+
 def gram(X):
     num_channels, n = X.shape[1], X.size // X.shape[1]
     X = X.reshape((num_channels, n))
@@ -82,7 +85,7 @@ def compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram):
     tv_l = tv_loss(X) * tv_weight
     # Add up all the losses
     l = sum(10 * styles_l + contents_l + [tv_l])
-    return contents_l, styles_l, tv_l, l
+    return l
 
 
 # Initializing the Synthesized Image
@@ -110,8 +113,7 @@ def train(X, contents_Y, styles_Y, device, lr, num_epochs, lr_decay_epoch):
         with autograd.record():
             contents_Y_hat, styles_Y_hat = extract_features(
                 X, content_layers, style_layers)
-            contents_l, styles_l, tv_l, l = compute_loss(
-                X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram)
+            l = compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram)
         l.backward()
         trainer.step(1)
         if (epoch + 1) % lr_decay_epoch == 0:
@@ -125,3 +127,6 @@ net.reset_device(device)
 content_X, contents_Y = get_contents(image_shape, device)
 _, styles_Y = get_styles(image_shape, device)
 output = train(content_X, contents_Y, styles_Y, device, 0.9, 500, 50)
+
+pil_image = array_to_pil(output)
+pil_image.show()
